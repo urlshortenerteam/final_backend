9e9d3c4f9d0c10a143d4cda23905c3a3c25f5556
violedo <liumianzhi@qq.com>
2020-07-07 15:00:46 +0800

    changed url.sql

--- database/url.sql
+++ database/url.sql
@@ -4,7 +4,7 @@ drop table if exists shorten_log;
 drop table if exists users;
 create table users
 (
-	id int auto_increment,
+	id bigint auto_increment,
 	name varchar(50) not null,
 	password varchar(50) not null,
 	primary key (id),
@@ -12,16 +12,15 @@ create table users
 );
 create table shorten_log
 (
-	id int auto_increment,
-	short varchar(30) not null,
-	creator_id int not null,
+	id bigint auto_increment,
+	creator_id bigint not null,
 	create_time datetime not null,
 	primary key (id),
 	foreign key (creator_id) references users(id) on delete cascade
 );
 create table visit_log
 (
-	id int auto_increment,
+	id bigint auto_increment,
 	shortener_id varchar(24) not null,
 	visit_time datetime not null,
 	ip varchar(15) not null,
@@ -30,10 +29,9 @@ create table visit_log
 );
 create table edit_log
 (
-	id int auto_increment,
+	id bigint auto_increment,
 	editor_id varchar(50) not null,
 	edit_time datetime not null,
-	shorten_id int not null,
-	primary key (id),
-	foreign key (shorten_id) references shorten_log(id) on delete cascade
+	shortener_id varchar(24) not null,
+	primary key (id)
 );
