3b37021c9ee41e713c34769d4db60a68b0873a4a
violedo <liumianzhi@qq.com>
2020-07-08 15:34:25 +0800

    fixed dependency of org.lionsoul.Still can't run.

--- backend/src/main/java/org/reins/url/entity/Statistics.java
+++ backend/src/main/java/org/reins/url/entity/Statistics.java
@@ -1,7 +1,6 @@
 package org.reins.url.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.lionsoul.ip2region.*;
 
 import java.io.IOException;
@@ -26,32 +25,32 @@ public class Statistics {
     }
 
     public void addArea_distr(String ip) throws Exception {
-//        boolean isIpAddress = Util.isIpAddress(ip);
-//        if (!isIpAddress)
-//            return;
-//
-//        //long ipLong = Util.ip2long(ip);
-//        //String strIp = Util.long2ip(ipLong);
-//
-//        DbConfig config = new DbConfig();
-//        String dbfile = "./src/main/resources/ip2region.db";
-//        DbSearcher searcher = new DbSearcher(config, dbfile);
-//        DataBlock block = searcher.btreeSearch(ip);
-//        String area=block.getRegion().substring(6,8);
-//        boolean flag=false;
-//        for (Area_distr area_distr:area_distrs){
-//            if (area_distr.name.equals(area)){
-//                ++area_distr.value;
-//                flag=true;
-//                break;
-//            }
-//        }
-//        if (!flag){
-//            Area_distr a=new Area_distr();
-//            a.name=area;
-//            a.value=1;
-//            area_distrs.add(a);
-//        }
+        boolean isIpAddress = Util.isIpAddress(ip);
+        if (!isIpAddress)
+            return;
+
+        //long ipLong = Util.ip2long(ip);
+        //String strIp = Util.long2ip(ipLong);
+
+        DbConfig config = new DbConfig();
+        String dbfile = "./src/main/resources/ip2region.db";
+        DbSearcher searcher = new DbSearcher(config, dbfile);
+        DataBlock block = searcher.btreeSearch(ip);
+        String area=block.getRegion().substring(6,8);
+        boolean flag=false;
+        for (Area_distr area_distr:area_distrs){
+            if (area_distr.name.equals(area)){
+                ++area_distr.value;
+                flag=true;
+                break;
+            }
+        }
+        if (!flag){
+            Area_distr a=new Area_distr();
+            a.name=area;
+            a.value=1;
+            area_distrs.add(a);
+        }
     }
 
     public void addTime_distr(Date visit_time){
--- backend/src/main/java/org/reins/url/entity/Time_distr.java
+++ backend/src/main/java/org/reins/url/entity/Time_distr.java
@@ -1,10 +1,8 @@
 package org.reins.url.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Time_distr {
     public int time;
     public long value;
