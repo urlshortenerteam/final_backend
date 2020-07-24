61d7653dd2b9c849cb42fed1bd391505318435f8
zhanghanchong <308011618@qq.com>
2020-07-24 09:57:19 +0800

    fix: StatControllerTest

--- backend/src/test/java/org/reins/url/StatControllerTest.java
+++ backend/src/test/java/org/reins/url/StatControllerTest.java
@@ -267,6 +267,7 @@ public class StatControllerTest extends ApplicationTests {
         VisitLog visitLog = new VisitLog();
         visitLog.setShortenerId("000000000000000000000000");
         List<VisitLog> visitLogList = new ArrayList<>();
+        visitLog.setVisitTime(new Date());
         for (int i = 0; i < 6; i++) visitLogList.add(visitLog);
         when(visitLogRepository.findAllOrderByVisitTime()).thenReturn(visitLogList);
         Shortener shortener = new Shortener();
