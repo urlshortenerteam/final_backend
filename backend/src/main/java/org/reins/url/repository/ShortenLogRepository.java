ad47082457fc0b96a23157e4d24ea9c56d9b3b0c
violedo <liumianzhi@qq.com>
2020-07-31 13:22:47 +0800

    fix: bug in getStatPageable

--- backend/src/main/java/org/reins/url/repository/ShortenLogRepository.java
+++ backend/src/main/java/org/reins/url/repository/ShortenLogRepository.java
@@ -10,14 +10,14 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ShortenLogRepository extends JpaRepository<ShortenLog, Long> {
+
     Page<ShortenLog> findAll(Pageable pageable);
 
     @Query("select s from ShortenLog s where s.creatorId=:creatorId")
     List<ShortenLog> findByCreatorId(@Param("creatorId") long creatorId);
 
     @Query(value="select s from ShortenLog s where s.creatorId=:creatorId",
-            countQuery = "select count(*) from ShortenLog s where s.creatorId=:creatorId",
-            nativeQuery = true)
+            countQuery = "select count(s) from ShortenLog s where s.creatorId=:creatorId")
     Page<ShortenLog> findByCreatorId(@Param("creatorId") long creatorId,Pageable pageable);
 
     @Query("select s from ShortenLog s where s.shortUrl=:shortUrl")
