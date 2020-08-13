b793723dc4fbf9363c01640152226beaff93c600
zhanghanchong <308011618@qq.com>
2020-08-13 16:09:38 +0800

    fix: redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,3 @@
 #!bin/bash
 docker pull https://hub.docker.com/repository/docker/reevoo2020/backend:latest
-docker kill backend
-docker kill prometheus
-docker kill grafana
-docker kill cadvisor
-docker kill redis
 docker system prune -f
-docker-compose up -d
