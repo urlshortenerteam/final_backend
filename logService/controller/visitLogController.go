05ec6d39bedd479187546b26f2a0d82348fa6b56
violedo <liumianzhi@qq.com>
2020-08-25 17:01:22 +0800

    fix: bug in logService of CI

--- logService/controller/visitLogController.go
+++ logService/controller/visitLogController.go
@@ -1,24 +1,27 @@
 package controller
 
 import (
-	isrv "github.com/violedo/logService/interface/service"
-	log "github.com/sirupsen/logrus"
 	"net/http"
 	"sync"
+
+	log "github.com/sirupsen/logrus"
+	isrv "github.com/violedo/logService/interface/service"
+
 	//"encoding/json"
 	"regexp"
 	"strings"
 )
+
 //VisitLogController the visitLog controller
 type VisitLogController struct {
 	visitLogService isrv.ILogService
 }
 
 type postBody struct {
-	Device bool `json:"device"`
-	OwnerID uint64 `json:"ownerId"`
-	ShortenID uint64 `json:"shortenId"`
-	IP string `json:"ip"`
+	Device      bool   `json:"device"`
+	OwnerID     uint64 `json:"ownerId"`
+	ShortenID   uint64 `json:"shortenId"`
+	IP          string `json:"ip"`
 	ShortenerID string `json:"shortenerId"`
 }
 
@@ -27,7 +30,7 @@ func (v *VisitLogController) Init(wg *sync.WaitGroup, visitLogService isrv.ILogS
 	srv := &http.Server{Addr: ":9092"}
 	v.visitLogService = visitLogService
 	v.visitLogService.InitService()
-	http.HandleFunc("/redirect", v.serveLog) // 设置访问的路由
+	http.HandleFunc("/visitLog", v.serveLog) // 设置访问的路由
 	go func() {
 		defer wg.Done() // let main know we are done cleaning up
 
@@ -46,25 +49,25 @@ func (v *VisitLogController) serveLog(w http.ResponseWriter, r *http.Request) {
 	// var params postBody
 	// decoder.Decode(&params)
 	// v.visitLogService.Log(params.ShortenerID,params.IP,params.Device,params.OwnerID,params.ShortenID)
-	regPath:=regexp.MustCompile(`[A-Za-z0-9]{6}`)
+	regPath := regexp.MustCompile(`[A-Za-z0-9]{6}`)
 	log.Info(r.URL.Path[1:])
-	if regPath.MatchString(r.URL.Path[1:]) == false{
-		return 
+	if regPath.MatchString(r.URL.Path[1:]) == false {
+		return
 	}
 
 	var ip string
 	forwarded := r.Header.Get("X-FORWARDED-FOR")
-    if forwarded != "" {
-        ip = forwarded
-    }
+	if forwarded != "" {
+		ip = forwarded
+	}
 	ip = r.RemoteAddr
 	log.Info(ip)
 	userAgent := r.UserAgent()
 	var device bool
-	if strings.Index(userAgent,"Windows")>-1||strings.Index(userAgent,"Linux")>-1||strings.Index(userAgent,"Mac")>-1 {
+	if strings.Index(userAgent, "Windows") > -1 || strings.Index(userAgent, "Linux") > -1 || strings.Index(userAgent, "Mac") > -1 {
 		device = false
 	} else {
 		device = true
 	}
-	v.visitLogService.Log(r.URL.Path[1:],ip,device)
+	v.visitLogService.Log(r.URL.Path[1:], ip, device)
 }
