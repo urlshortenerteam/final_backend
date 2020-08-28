b427e5f1b27fd10811bd6b84880cc2a80a0f40fc
zhanghanchong <308011618@qq.com>
2020-08-28 11:31:01 +0800

    fix: source in getReal

--- backend/src/main/java/org/reevoo/url/repository/VisitLogRepository.java
+++ backend/src/main/java/org/reevoo/url/repository/VisitLogRepository.java
@@ -4,20 +4,15 @@ import org.reevoo.url.entity.VisitLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface VisitLogRepository extends JpaRepository<VisitLog, Long> {
-//    @Query("select v from VisitLog v order by v.visitTime desc")
-//    List<VisitLog> findAllOrderByVisitTime();
-
+    @Query("select v from VisitLog v where v.shortenerId=:shortenerId")
+    List<VisitLog> findByShortenerId(@Param("shortenerId") String shortenerId);
 
     @Query(value = "select v from VisitLog v order by v.visitTime desc",
             countQuery = "select count(v) from VisitLog v order by v.visitTime desc")
     List<VisitLog> findOrderByVisitTimeDesc(Pageable pageable);
-
-    @Query("select v from VisitLog v where v.shortenerId=:shortenerId")
-    List<VisitLog> findByShortenerId(@Param("shortenerId") String shortenerId);
 }
--- backend/src/main/java/org/reevoo/url/service/VisitLogService.java
+++ backend/src/main/java/org/reevoo/url/service/VisitLogService.java
@@ -2,6 +2,7 @@ package org.reevoo.url.service;
 
 import org.reevoo.url.entity.VisitLog;
 import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
