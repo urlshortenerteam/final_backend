4c51bf454a68c4329d5e394629b973079eac4025
violedo <liumianzhi@qq.com>
2020-08-28 10:57:31 +0800

    fix:mistake in Dockerfile of logService and performance optimization of logService

--- logService/controller/visitLogController.go
+++ logService/controller/visitLogController.go
@@ -95,18 +95,21 @@ func (v *VisitLogController) ServeLog() {
 
 			switch e := ev.(type) {
 			case *kafka.Message:
-				log.WithFields(log.Fields{
-					"topicPartition": e.TopicPartition,
-					"messageValue":string(e.Value),
-				}).Info("Kafka Message.")
-				json.Unmarshal(e.Value,&message)
-				var device bool
-				if strings.Index(message.UserAgent, "Windows") > -1 || strings.Index(message.UserAgent, "Linux") > -1 || strings.Index(message.UserAgent, "Mac") > -1 {
-					device = false
-				} else {
-					device = true
-				}
-				v.visitLogService.Log(message.ShortenID,message.LongID, message.IP, device)
+				go func() {
+					log.WithFields(log.Fields{
+						"topicPartition": e.TopicPartition,
+						"messageValue":string(e.Value),
+					}).Info("Kafka Message.")
+					json.Unmarshal(e.Value,&message)
+					var device bool
+					if strings.Index(message.UserAgent, "Windows") > -1 || strings.Index(message.UserAgent, "Linux") > -1 || strings.Index(message.UserAgent, "Mac") > -1 {
+						device = false
+					} else {
+						device = true
+					}
+					v.visitLogService.Log(message.ShortenID,message.LongID, message.IP, device)
+				}()
+
 			case kafka.Error:
 				// Errors should generally be considered
 				// informational, the client will try to
